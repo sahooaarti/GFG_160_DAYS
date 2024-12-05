4bdbfa22714ffe11e8003bbe6503c625bf0ae601
sahooaarti <aarti.sahoo@edpi.ai>
2024-12-05 23:13:28 +0530

    feat : Day 3 unlocked

--- Day2.py
+++ Day2.py
@@ -1,4 +1,4 @@
-#User function Template for python3
+# Given an array arr[]. Push all the zeros of the given array to the right end of the array while maintaining the order of non-zero elements. Do the mentioned change in the array in place.
 
 # class Solution:
 # 	def pushZerosToEnd(self,arr):
